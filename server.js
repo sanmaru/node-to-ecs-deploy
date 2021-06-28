21e456ef5a62f345ecf09cd0587614fd5e2a228a
Sanmaru (산마루) <75458598+sanmaru@users.noreply.github.com>
2021-06-28 15:23:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0"
 // App
 const app = express()
 app.get("/", (req, res) => {
-  res.send(`Hello ECS World2 - ${new Date().toISOString()}`)
+  res.send(`Hello ECS World Test - ${new Date().toISOString()}`)
 })
 
 app.listen(PORT, HOST)
